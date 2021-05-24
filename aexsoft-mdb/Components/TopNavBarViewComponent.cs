6c3915ff48060f46f793e5b57798935eb232519c
Ildar Tagiev <tag.ildar@gmail.com>
2021-05-24 22:02:43 +0300

    navigation menu component created

---
+++ aexsoft-mdb/Components/TopNavBarViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using aexsoftmdb.Models.ViewModels;
+
+namespace aexsoftmdb.Components
+{
+	public class TopNavBarViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.SelectedMenu = RouteData?.Values;
+			return View(NavigationMenuViewModel.GetNavigationMenuModels());
+		}
+	}
+}
---
+++ aexsoft-mdb/Models/ViewModels/NavigationMenuViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aexsoftmdb.Models.ViewModels
+{
+	/// <summary>
+	/// Dummy data for navigation menu.
+	/// </summary>
+	public class NavigationMenuViewModel
+	{
+		private NavigationMenuModel[] menuModels;
+
+		public NavigationMenuViewModel()
+		{
+			menuModels = new NavigationMenuModel[]
+			{
+				new NavigationMenuModel
+				{
+					Controller = "Home",
+					Action = "Index",
+					Name = "Home"
+				},
+				new NavigationMenuModel
+				{
+					Controller = "Home",
+					Action = "Search",
+					Name = "Search"
+				}
+			};
+		}
+
+		public static NavigationMenuModel[] GetNavigationMenuModels()
+		{
+			return new NavigationMenuViewModel().menuModels;
+		}
+	}
+
+	/// <summary>
+	/// Data model for menu element.
+	/// </summary>
+	public class NavigationMenuModel
+	{
+		public string Controller { get; set; }
+		public string Action { get; set; }
+		public string Name { get; set; }
+	}
+}
